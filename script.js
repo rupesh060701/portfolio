1a249a12f5db0b7c8882d9e78ffabc14f712216f
Rupesh Kumar <128151254+rupesh060701@users.noreply.github.com>
2024-02-26 22:55:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+var textWrapper = document.querySelector('.name');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.name .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.name',
+    opacity: 1,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
